2260e38af9c070c59cfb52dc4f5a1e92022d1a79
Daniel-Jangua <daniel.jangua@unesp.br>
2020-06-20 00:24:36 -0300

    Adicionada operações com vetores e matrizes que serão usadas nos métodos

--- VectorOperations.java
+++ VectorOperations.java
@@ -15,4 +15,74 @@ public abstract class VectorOperations {
             result.add(x.get(i)*escalar);
         return result;
     }
+
+    //v^t*v = escalar
+    public static double multVecTxVec(Vector<Double> v1t, Vector<Double>v2){
+        double result = 0;
+        for(int i = 0; i < v1t.size(); i++)
+            result += v1t.get(i)*v2.get(i);
+        return result;
+    }
+
+    //v*v^t=M
+    public static Vector<Vector<Double>> multiVecxVecT(Vector<Double> v1, Vector<Double>v2t){
+        Vector<Vector<Double>> result = new Vector<Vector<Double>>();
+        for(int i = 0; i < v1.size(); i++){
+            Vector<Double> aux = new Vector<Double>();
+            for(int j = 0; j < v2t.size(); j++)
+                aux.add(v1.get(i)*v2t.get(j));
+            result.add(aux);
+        }
+        return result;
+    }
+
+    //M*v=v
+    public static Vector<Double> multiMatrizxVec(Vector<Vector<Double>> m, Vector<Double> v){
+        Vector<Double> result = new Vector<Double>();
+        for(int linha = 0; linha < m.size(); linha++){
+            double soma = 0;
+            for(int col = 0; col < m.get(linha).size(); col++)
+                soma += m.get(linha).get(col)*v.get(col);
+            result.add(soma);
+        }
+        return result;
+    }
+
+    //v^t*M=v^t -> considerar o vetor resultante como um vetor transposto
+    public static Vector<Double> multiVecTxMatriz(Vector<Double> v, Vector<Vector<Double>> m){
+        Vector<Double> result = new Vector<Double>();
+        for(int i = 0; i < m.get(0).size(); i++){
+            double soma = 0;
+            for(int j = 0; j < v.size(); j++)
+                soma += v.get(j)*m.get(j).get(i);
+            result.add(soma);
+        }
+        return result;
+    }
+
+    //M*k=M
+    public static Vector<Vector<Double>> multiMatrizEscalar(Vector<Vector<Double>> m, double escalar){
+        Vector<Vector<Double>> result = new Vector<Vector<Double>>();
+        for(int i = 0; i < m.size(); i++){
+            Vector<Double> aux = new Vector<Double>();
+            for(int j=0; j < m.get(i).size(); j++){
+                aux.add(m.get(i).get(j)*escalar);
+            }
+            result.add(aux);
+        }
+        return result;
+    }
+
+    public static Vector<Vector<Double>> somaMatrizes(Vector<Vector<Double>> m1, Vector<Vector<Double>> m2){
+        Vector<Vector<Double>> result = new Vector<Vector<Double>>();
+        for(int i = 0; i < m1.size(); i++){
+            Vector<Double> aux = new Vector<Double>();
+            for(int j = 0; j < m1.get(i).size(); j++){
+                aux.add(m1.get(i).get(j)+m2.get(i).get(j));
+            }
+            result.add(aux);
+        }
+        return result;
+    }
+
 }
